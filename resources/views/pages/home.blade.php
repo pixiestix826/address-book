16cfbfb9e3082a4cf210f147c638b41d3088d0dc
Brandi Soucy <bbsoucy@gmail.com>
2016-02-15 08:27:34 -0500

    generate main page template

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="app.css" media="screen">
-        <title>Address Book</title>
-    </head>
-    <body>
-        <nav class=top-nav>
-            <a href="/">View Contacts</a>
-        </nav>
-        <div class="">
-            <h1>Welcome</h1>
+@extends('templates.main')
+@section('content')
 
-        </div>
-    </body>
-</html>
+<h1>Address Book</h1>
+
+<div>
+    <button type="button">
+        <a href="{{ route('contacts.index') }}">View Address Book</a>
+    </button>
+    <button type="button">
+        <a href="{{ route('contacts.create') }}">Create A New Contact</a>
+    </button>
+</div>
+
+@endsection
---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="app.css" media="screen">
+        <title>Address Book</title>
+    </head>
+    <body>
+
+        <div class="content">
+            @@yield('content')
+        </div>
+
+    </body>
+</html>
