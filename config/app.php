ddda4f21f1e54641a38b78b7dad7ac6cf4eb3813
Brandi Soucy <bbsoucy@gmail.com>
2016-02-15 07:38:14 -0500

    updating errors on config file

--- config/app.php
+++ config/app.php
@@ -173,8 +173,8 @@ return [
     */
 
     'aliases' => [
-        'Form'      => Collective\Html\Facade\Form::class,
-        'Html'      => Collective\Html\Facade\Html::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
         'App'       => Illuminate\Support\Facades\App::class,
         'Artisan'   => Illuminate\Support\Facades\Artisan::class,
         'Auth'      => Illuminate\Support\Facades\Auth::class,
