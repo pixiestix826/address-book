9d859a5fa2448b393fb4b5804e03b0725e7c5f9b
Brandi Soucy <bbsoucy@gmail.com>
2016-02-15 01:13:01 -0500

    created a welcome controller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -7,10 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class PagesController extends Controller {
-
-    public function home()
-    {
-        return 'Welcome';
-    }
+class WelcomeController extends Controller
+{
+    //
 }
